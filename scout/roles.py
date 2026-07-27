7cadd5037a1d42c9d9a4e3eb4f9a2a7714c93ae3
Brandnwalters <bmwal@unc.edu>
2026-07-27 12:50:10 -0400

    Added desired roles to roles.py

---
+++ scout/roles.py
@@ -0,0 +1,35 @@
+
+ROLES = {
+    "advanced_forward": {
+        "Fin" : 3, "Cmp" : 2, "Pac" : 2,
+        "Acc" : 2, "OtB" : 3
+    },
+
+    "ball_playing_cb": {
+        "Pac": 2, "Acc": 2, "Dec" : 2,
+        "Pos" : 3, "Pas" : 2, "Tck": 3
+    },
+
+    "winger":  {
+        "Fin": 2, "Acc": 3, "Pac": 3,
+        "OtB": 2, "Dri": 2, "Fir": 2
+    },
+
+     "deep_lying_playmaker": {
+
+        "Passing": 3, "Vision": 3, "Composure": 3, "First Touch": 3,
+
+        "Decisions": 2, "Technique": 2, "Anticipation": 2,
+
+        "Teamwork": 1, "Stamina": 1,},
+
+    "ball_winning_midfielder": {
+
+        "Tackling": 3, "Aggression": 3, "Work Rate": 3, "Stamina": 3,
+
+        "Anticipation": 2, "Positioning": 2, "Teamwork": 2,
+
+        "Strength": 1, "Passing": 1,
+
+    }
+}
